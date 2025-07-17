e5740a0a19a346f6e1a0d0be8829ca550311392b
Niccolò Turicci <162383365+niccolo-turicci@users.noreply.github.com>
2025-07-17 11:54:41 +0200

    Update __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+from .conversion import *
+
+def main():
+    parser = argparse.ArgumentParser(description="Process AlphaFold output and convert to standardized formats.")
+    parser.add_argument('--input', type=str, required=True, help="Path to AlphaFold output directory.")
+    parser.add_argument('--output', type=str, required=True, help="Base name for output directories.")
+    args = parser.parse_args()
+
+
+# create temporary folder with intermediate files and output folder
+temp_folder = os.path.join(os.getcwd(), args.output, "temp_files")
+output_folder = os.path.join(os.getcwd(), args.output, "output_folder")
+os.makedirs(temp_folder, exist_ok=True)
+os.makedirs(output_folder, exist_ok=True)
+
+
+# here are the functions: defined in conversion.py
+output_residue_distances(args.input, temp_folder)
+output_contact_probabilities(args.input, temp_folder)
+output_full_data(args.input, temp_folder, output_folder)
+output_summary_confidence(args.input, output_folder)
+output_job_request(args.input, output_folder)
+convert_pdb_to_cif(args.input, output_folder)
+
+# actual main
+if __name__ == "__main__":
+    main()
