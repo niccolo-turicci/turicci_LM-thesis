fefa520efe28badade0ccc92d9250397903c499b
Niccolò Turicci <162383365+niccolo-turicci@users.noreply.github.com>
2025-07-17 18:32:48 +0200

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -4,8 +4,11 @@ from .conversion import *
 
 def main():
     parser = argparse.ArgumentParser(description="Takes APD output and makes it suited to be fed into ABridge.")
+
+    # arguments after "python -m src"
     parser.add_argument('--input', type=str, required=True, help="Path to the input folder containing the APD output.")
     parser.add_argument('--output', type=str, required=True, help="Name for output directory (both temporary and output folders are gonna be here.")
+    
     args = parser.parse_args()
 
 
