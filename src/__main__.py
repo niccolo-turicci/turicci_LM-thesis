be064024b003bfdb54913d3e79e707b430770bc9
Niccolò Turicci <162383365+niccolo-turicci@users.noreply.github.com>
2025-07-17 18:12:40 +0200

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -9,20 +9,20 @@ def main():
     args = parser.parse_args()
 
 
-# create temporary folder with intermediate files and output folder
-temp_folder = os.path.join(os.getcwd(), args.output, "temp_files")
-output_folder = os.path.join(os.getcwd(), args.output, "output_folder")
-os.makedirs(temp_folder, exist_ok=True)
-os.makedirs(output_folder, exist_ok=True)
+    # create temporary folder with intermediate files and output folder
+    temp_folder = os.path.join(os.getcwd(), args.output, "temp_files")
+    output_folder = os.path.join(os.getcwd(), args.output, "output_folder")
+    os.makedirs(temp_folder, exist_ok=True)
+    os.makedirs(output_folder, exist_ok=True)
 
 
-# here are the functions: defined in conversion.py
-output_residue_distances(args.input, temp_folder)
-output_contact_probabilities(args.input, temp_folder)
-output_full_data(args.input, temp_folder, output_folder)
-output_summary_confidence(args.input, output_folder)
-output_job_request(args.input, output_folder)
-convert_pdb_to_cif(args.input, output_folder)
+    # here are the functions: defined in conversion.py
+    output_residue_distances(args.input, temp_folder)
+    output_contact_probabilities(args.input, temp_folder)
+    output_full_data(args.input, temp_folder, output_folder)
+    output_summary_confidence(args.input, output_folder)
+    output_job_request(args.input, output_folder)
+    convert_pdb_to_cif(args.input, output_folder)
 
 # actual main
 if __name__ == "__main__":
